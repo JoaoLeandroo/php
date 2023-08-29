b231a2f202b9135743d5122499318c0742f91e72
joaoleandro <new.joaoleandro@gmail.com>
2023-08-29 10:24:39 -0300

    estudando variaveis em php

---
+++ variaveis/index.php
@@ -0,0 +1,21 @@
+<!-- Tomar cuidado com o camelCase -->
+
+<?php
+    // Variavel com a letra minuscula
+    $var = "vamos nessa";
+    
+    // Variavel com a letra maiuscul
+    $VAR = "teste";
+
+    echo "$var, $VAR";
+?>
+
+<br>
+
+<?php
+    $name_user = "joao";
+    $age_user = 24;
+    $city_user = 'Recife';
+
+    echo "$name_user, mora em $city_user e tem $age_user anos.";
+?>
